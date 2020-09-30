34618f1dfd63ae712da2dbfd15637603ba642338
yusuf <yesufbushira9@gmail.com>
2020-09-30 16:24:20 -0500

    Cart Icon

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { View, Text, ScrollView } from 'react-native';
-import { Card } from 'react-native-elements';
+import { View, Text, ScrollView ,StyleSheet } from 'react-native';
+import { Card ,Icon} from 'react-native-elements';
 import {PRODUCTS} from '../shared/products';
 
 
-function RenderItem({item}) {
+function RenderItem(props) {
+    const  {item}=props;
     if (item) {
         return (
             <Card
@@ -14,6 +15,26 @@ function RenderItem({item}) {
                     style={{margin: 10}}>
                     {item.ProductDescription}
                 </Text>
+                <View style={styles.cardRow}>
+                <Icon  
+                    name={props.favorite ? 'heart' : 'heart-o'}
+                    type='font-awesome'
+                    color='#f50'
+                    raised
+                    reverse
+                    onPress={() => props.favorite ? 
+                        console.log('Already set as a favorite') : props.markFavorite()}
+                />
+                <Icon  style={styles.cardItem}
+                    name='shopping-basket' 
+                    type='font-awesome'
+                    color='#f50'
+                    raised
+                    reverse
+                    onPress={() => 'shopping-basket'  ? props.addToCart() :
+                        console.log('Already in cart') }
+                />
+                </View>
             </Card>
         );
     }
@@ -24,7 +45,8 @@ class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            products: PRODUCTS
+            products: PRODUCTS,
+            favorite: false
             
         };
     }
@@ -32,15 +54,36 @@ class Home extends Component {
     static navigationOptions = {
         title: 'Home'
     }
-
+    markFavorite() {
+        this.setState({favorite: true});
+    }
+    addToCart() {
+        console.log('Already in cart');
+    }
     render() {
         return (
             <ScrollView>
                 <RenderItem 
-                    item={this.state.products.filter(product=> product.featured)[0]} />
+                    item={this.state.products.filter(product=> product.featured)[0]} 
+                    favorite={this.state.favorite}
+                    markFavorite={() => this.markFavorite()}
+                    addToCart={() => this.addToCart()}
+                    />
             </ScrollView>
-        ); featured
+        ); 
     }
 }
-
+const styles = StyleSheet.create({
+    cardRow: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        flexDirection: 'row',
+        margin: 20
+    },
+    cardItem: {
+        flex: 1,
+        margin: 10
+    }
+});
 export default Home;
--- components/ProductDetailComponent.js
+++ components/ProductDetailComponent.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
-import { Card } from 'react-native-elements';
+import { Text, View,StyleSheet } from 'react-native';
+import { Card, Icon } from 'react-native-elements';
 import {PRODUCTS} from '../shared/products';
 
 
-function RenderProduct({product}) {
+function RenderProduct(props)
+ {    const  {product}=props;
     if (product) {
         return (
             <Card
@@ -13,6 +14,26 @@ function RenderProduct({product}) {
                 <Text style={{margin: 10}}>
                     {product.ProductDescription}
                 </Text>
+                <View style={styles.cardRow}>
+                <Icon  
+                    name={props.favorite ? 'heart' : 'heart-o'}
+                    type='font-awesome'
+                    color='#f50'
+                    raised
+                    reverse
+                    onPress={() => props.favorite ? 
+                        console.log('Already set as a favorite') : props.markFavorite()}
+                />
+                <Icon  style={styles.cardItem}
+                    name='shopping-basket' 
+                    type='font-awesome'
+                    color='#f50'
+                    raised
+                    reverse
+                    onPress={() => 'shopping-basket'  ? props.addToCart() :
+                        console.log('Already in cart') }
+                />
+                </View>
             </Card>
         );
     }
@@ -25,18 +46,41 @@ class  SingleProduct extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            products:PRODUCTS
+            products:PRODUCTS,
+            favorite: false
         };
     }
 
     static navigationOptions = {
         title: 'Product Information'
     }
-
+    markFavorite() {
+        this.setState({favorite: true});
+    }
+    addToCart() {
+        console.log('Already in cart');
+    }
     render() {
         const productId = this.props.navigation.getParam('productId');
         const product = this.state.products.filter(product => product.id===productId)[0];
-        return <RenderProduct product={product}/>;
+        return <RenderProduct product={product}
+        favorite={this.state.favorite}
+        markFavorite={() => this.markFavorite()}
+        addToCart={() => this.addToCart()}
+        />;
     }
 }
+const styles = StyleSheet.create({
+    cardRow: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        flexDirection: 'row',
+        margin: 20
+    },
+    cardItem: {
+        flex: 1,
+        margin: 10
+    }
+});
 export default  SingleProduct;
