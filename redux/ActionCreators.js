1bdad3b6db343840d51edde9f85fd0071b56be31
yusuf <yesufbushira9@gmail.com>
2020-09-30 19:06:51 -0500

    Setting up Redux1

---
+++ redux/ActionCreators.js
@@ -0,0 +1,68 @@
+import * as ActionTypes from './ActionTypes';
+import { baseUrl } from '../shared/baseUrl';
+export const fetchComments = () => dispatch => {
+    return fetch(baseUrl + 'comments')
+        .then(response => {
+                if (response.ok) {
+                    return response;
+                } else {
+                    const error = new Error(`Error ${response.status}: ${response.statusText}`);
+                    error.response = response;
+                    throw error;
+                }
+            },
+            error => {
+                const errMess = new Error(error.message);
+                throw errMess;
+            })
+        .then(response => response.json())
+        .then(comments => dispatch(addComments(comments)))
+        .catch(error => dispatch(commentsFailed(error.message)));
+};
+
+export const commentsFailed = errMess => ({
+    type: ActionTypes.COMMENTS_FAILED,
+    payload: errMess
+});
+
+export const addComments = (comments) => ({
+    type: ActionTypes.ADD_COMMENTS,
+    payload: comments
+});
+
+export const fetchProducts = () => dispatch => {
+
+    dispatch(productsLoading());
+
+    return fetch(baseUrl + 'campsites')
+        .then(response => {
+                if (response.ok) {
+                return response;
+                } else {
+                    const error = new Error(`Error ${response.status}: ${response.statusText}`);
+                    error.response = response;
+                throw error;
+                }
+            },
+            error => {
+                const errMess = new Error(error.message);
+                throw errMess;
+            })
+        .then(response => response.json())
+        .then(products => dispatch(addProducts(products)))
+        .catch(error => dispatch(productsFailed(error.message)));
+};
+
+export const productsLoading = () => ({
+    type: ActionTypes.PRODUCTS_LOADING
+});
+
+export const productsFailed = errMess => ({
+    type: ActionTypes.PRODUCTS_FAILED,
+    payload: errMess
+});
+
+export const addProducts = products => ({
+    type: ActionTypes.ADD_PRODUCTS,
+    payload:products
+});
